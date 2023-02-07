56ed6d979fd7d8b4c9fc26c220e97544261e6cb5
paramshah1903 <96541661+paramshah1903@users.noreply.github.com>
2023-02-07 22:33:13 +0530

    basic layout

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import CAS from "./Components/CAS";
+
+function App() {
+  return (
+    <div className="App">
+      <CAS />
+    </div>
+  );
+}
+
+export default App;
