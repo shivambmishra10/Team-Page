1b8a331225036eee4bcd19ca5e554e0e6d2bdb6a
paramshah1903 <96541661+paramshah1903@users.noreply.github.com>
2023-02-22 22:26:08 +0530

    infinite scrollbar

--- Components/Container.js
+++ Components/Container.js
@@ -1,8 +1,30 @@
 import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 import "./Container.css";
 
 import ProfileCard from "./ProfileCard.js";
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 const Container = () => {
   return (
@@ -17,16 +39,20 @@ const Container = () => {
               classSpecial="profile__card cardSpecial"
             />
           </div>
-
-          <div className="joints__parent" id="joints">
-            <h1 className="heading">Core</h1>
-            <div className="joints__container">
+          <Carousel
+            responsive={responsive}
+            infinite={false}
+            removeArrowOnDeviceType="all"
+          >
+            <div>
               <ProfileCard
                 name="Aayushman Tiwari"
                 position="Joint Cultural Affairs Secretary"
                 imgSrc="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
                 classSpecial=" profile__card cardSmall"
               />
+            </div>
+            <div>
               <ProfileCard
                 name="Prashant Paliwal"
                 position="Joint Cultural Affairs Secretary"
@@ -34,13 +60,15 @@ const Container = () => {
                 classSpecial="  profile__card cardSmall"
               />
             </div>
-            <div className="joints__container">
+            <div>
               <ProfileCard
                 name="Kuldeep"
                 position="Joint Cultural Affairs Secretary"
                 imgSrc="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
                 classSpecial=" profile__card cardSmall"
               />
+            </div>
+            <div>
               <ProfileCard
                 name="Kanha Bhawani"
                 position="Joint Cultural Affairs Secretary"
@@ -48,8 +76,14 @@ const Container = () => {
                 classSpecial="  profile__card cardSmall"
               />
             </div>
-          </div>
+          </Carousel>
+          ;{/* <div className="joints__parent" id="joints"> */}
+          <h1 className="heading">Core</h1>
+          {/* 
+            <div className="joints__container"></div>
+            <div className="joints__container"></div> */}
         </div>
+        {/* </div> */}
       </>
     </div>
   );
--- Components/Sidebar.js
+++ Components/Sidebar.js
@@ -1,78 +1,128 @@
 import React from "react";
-
+import Drawer from "@mui/material/Drawer";
 import "./Sidebar.css";
 
+// const arrayList = [
+//   {
+//     id: 0,
+//     name: "nam",
+//   },
+//   {
+//     id: 1,
+//     name: "nam2",
+//   },
+//   {
+//     id: 2,
+//     name: "nam3",
+//   },
+// ];
+
 const Sidebar = () => {
+  const [open, setopen] = React.useState(false);
+  // const [slideIndex, setslideIndex] = React.useState(0);
   return (
-    <div className="sidebar">
-      <ul class="nav__links">
-        <li class="nav__item">
-          <a class="nav__link" href="#joints">
-            CORE
-          </a>
-        </li>
-        <li class="nav__item">
-          <a class="nav__link" href="#section--2">
-            INFRA AND IN HOUSE MANAGERIAL
-          </a>
-        </li>
-        <li class="nav__item">
-          <a class="nav__link" href="#section--3">
-            WEB DEVELOPMENT
-          </a>
-        </li>
-        <li class="nav__item">
-          <a class="nav__link" href="#section--3">
-            PUBLICITY
-          </a>
-        </li>
-        <li class="nav__item">
-          <a class="nav__link" href="#section--3">
-            SPONSORSHIP
-          </a>
-        </li>
-        <li class="nav__item">
-          <a class="nav__link" href="#section--3">
-            PUBLIC RELATION
-          </a>
-        </li>
-        <li class="nav__item">
-          <a class="nav__link" href="#section--3">
-            HOSPITALITY
-          </a>
-        </li>
-        <li class="nav__item">
-          <a class="nav__link" href="#section--3">
-            DISCIPLINE
-          </a>
-        </li>
-        <li class="nav__item">
-          <a class="nav__link" href="#section--3">
-            ADMIN AND DOCUMNETATION
-          </a>
-        </li>
-        <li class="nav__item">
-          <a class="nav__link" href="#section--3">
-            DESIGN
-          </a>
-        </li>
-        <li class="nav__item">
-          <a class="nav__link" href="#section--3">
-            THINK TANK
-          </a>
-        </li>
-        <li class="nav__item">
-          <a class="nav__link" href="#section--3">
-            MEDIA CELL
-          </a>
-        </li>
-        <li class="nav__item">
-          <a class="nav__link" href="#section--3">
-            CINEMATOGRAPHY
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      {/* <Drawer anchor={"right"} open={open} onClose={() => setopen(false)}>
+        <span>Hellow</span>
+      </Drawer> */}
+      <div className="sidebar">
+        {/* <button
+          style={{
+            zIndex: 100,
+            backgroundColor: "white",
+            width: "100px",
+            height: "100px",
+          }}
+          onClick={() => setopen(true)}
+        >
+          Hi
+        </button> */}
+        {/* <div
+          style={{
+            backgroundColor: "white",
+            color: "blue",
+          }}
+        >
+          {arrayList
+            ?.filter((al) => {
+              if (al.id == slideIndex) {
+                return al;
+              }
+            })
+            ?.map((al, index) => (
+              <>{al.id}</>
+            ))}
+        </div> */}
+
+        <ul class="nav__links">
+          <li class="nav__item">
+            <a class="nav__link" href="#joints">
+              CORE
+            </a>
+          </li>
+          <li class="nav__item">
+            <a class="nav__link" href="#section--2">
+              INFRA AND IN HOUSE MANAGERIAL
+            </a>
+          </li>
+          <li class="nav__item">
+            <a class="nav__link" href="#section--3">
+              WEB DEVELOPMENT
+            </a>
+          </li>
+          <li class="nav__item">
+            <a class="nav__link" href="#section--3">
+              PUBLICITY
+            </a>
+          </li>
+          <li class="nav__item">
+            <a class="nav__link" href="#section--3">
+              SPONSORSHIP
+            </a>
+          </li>
+          <li class="nav__item">
+            <a class="nav__link" href="#section--3">
+              PUBLIC RELATION
+            </a>
+          </li>
+          <li class="nav__item">
+            <a class="nav__link" href="#section--3">
+              HOSPITALITY
+            </a>
+          </li>
+          <li class="nav__item">
+            <a class="nav__link" href="#section--3">
+              DISCIPLINE
+            </a>
+          </li>
+          <li class="nav__item">
+            <a class="nav__link" href="#section--3">
+              ADMIN AND DOCUMNETATION
+            </a>
+          </li>
+          <li class="nav__item">
+            <a class="nav__link" href="#section--3">
+              DESIGN
+            </a>
+          </li>
+          <li class="nav__item">
+            <a class="nav__link" href="#section--3">
+              THINK TANK
+            </a>
+          </li>
+          <li class="nav__item">
+            <a class="nav__link" href="#section--3">
+              MEDIA CELL
+            </a>
+          </li>
+          <li class="nav__item">
+            <a class="nav__link" href="#section--3">
+              CINEMATOGRAPHY
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
